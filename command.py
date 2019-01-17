a5de3f5e724329f21a864ba433a52101fcc8b0a8
Roman Kovalyov <mati.green.13@gmail.com>
2019-01-17 20:33:57 +0200

    clean

--- command.py
+++
@@ -1,7 +0,0 @@
-from argparse import ArgumentParser
-
-command_sign = ':'
-
-top_parser = ArgumentParser()
-
-top_parser.add_argument('chat')
--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python3
 
 from client import client
-import handlers as handler
-import ui
-
-client.start()
-
-ui.msg_line.on_enter = handler.msg_line_on_enter
-ui.msg_line.on_key_press = handler.msg_line_on_key_press
-
-ui.cmd_line.on_enter = handler.cmd_line_on_enter
-ui.cmd_line.on_key_press = handler.cmd_line_on_key_press
-
-ui.window.run()
