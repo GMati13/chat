5dbd00731304f3301a0be1d8c0046f1b484553ac
Roman Kovalyov <mati.green.13@gmail.com>
2019-01-20 01:35:37 +0200

    fix crash, when chat not selectend and happen message

--- src/handlers/client/listeners.py
+++ src/handlers/client/listeners.py
@@ -6,8 +6,11 @@ import lib.urwid as urwid
 
 @client.on_message()
 def on_message(c, message):
-    chat_id = store.get_item('current_chat')['id']
-    chat_type = store.get_item('current_chat')['type']
+    chat = store.get_item('current_chat')
+    if chat is None:
+        return
+    chat_id = chat['id']
+    chat_type = chat['type']
     if message['chat']['id'] == chat_id:
         chat.append_message(message, chat_type)
         body.history.scroll_end()
